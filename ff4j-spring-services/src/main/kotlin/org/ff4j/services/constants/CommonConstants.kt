f594927c87cf2e4c4166ea8343e5b51a40288b96
Paul <paul58914080@gmail.com>
2018-05-02 22:02:44 +0530

    converted all the constants to kotlin from java

---
+++ ff4j-spring-services/src/main/kotlin/org/ff4j/services/constants/CommonConstants.kt
@@ -0,0 +1,11 @@
+package org.ff4j.services.constants
+
+/**
+ * Created by Paul
+ *
+ * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
+ */
+object CommonConstants {
+    const val N_A = "N/A"
+    const val HTML_WHITE = "FFFFFF"
+}
