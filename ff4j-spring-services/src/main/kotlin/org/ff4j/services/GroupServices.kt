62a92fca2fe7dbc874eba291bc350426493c9706
Paul <paul58914080@gmail.com>
2018-05-04 10:16:30 +0530

    converted GroupServices to kotlin from java

---
+++ ff4j-spring-services/src/main/kotlin/org/ff4j/services/GroupServices.kt
@@ -0,0 +1,34 @@
+package org.ff4j.services
+
+import org.ff4j.FF4j
+import org.ff4j.core.Feature
+import org.ff4j.services.domain.FeatureApiBean
+import org.ff4j.services.validator.FeatureValidator
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.stream.Collectors
+
+/**
+ * Created by Paul
+ *
+ * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
+ */
+@Service
+class GroupServices(@Autowired val fF4j: FF4j, @Autowired val ff4jValidator: FeatureValidator) {
+
+    fun getFeaturesByGroup(groupName: String): Collection<FeatureApiBean> {
+        ff4jValidator.assertGroupExist(groupName)
+        return fF4j.featureStore.readGroup(groupName).values.stream().map { feature: Feature -> FeatureApiBean(feature) }
+                .collect(Collectors.toList())
+    }
+
+    fun enableGroup(groupName: String) {
+        ff4jValidator.assertGroupExist(groupName)
+        fF4j.featureStore.enableGroup(groupName)
+    }
+
+    fun disableGroup(groupName: String) {
+        ff4jValidator.assertGroupExist(groupName)
+        fF4j.featureStore.disableGroup(groupName)
+    }
+}
