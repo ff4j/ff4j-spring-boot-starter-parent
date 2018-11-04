15a68f8f6cca995c76bac1a48557d9aeea24ef61
Paul <paul58914080@gmail.com>
2018-11-04 18:44:10 +0530

    Removed auto-configuration of web

--- ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/Application.java
+++ ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/Application.java
@@ -13,17 +13,16 @@
  */
 package org.ff4j.sample;
 
+import org.ff4j.spring.boot.autoconfigure.FF4JWebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by Paul
  *
  * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
  */
-@SpringBootApplication
-@ComponentScan(value = "org.ff4j.sample")
+@SpringBootApplication(exclude = FF4JWebConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
