3ea55235de1cad99cd85cc677c8beb3ea9ba44c4
paul58914080 <paul58914080@gmail.com>
2019-02-23 01:36:46 +0530

    fix(console): sample for console

--- ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/config/FF4JWebConfiguration.java
+++ ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/config/FF4JWebConfiguration.java
@@ -1,43 +1,44 @@
-/*
 package org.ff4j.sample.config;
 
 import org.ff4j.FF4j;
+import org.ff4j.web.FF4jDispatcherServlet;
 import org.ff4j.web.embedded.ConsoleServlet;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-
-*/
-/**
- * Created by Paul
- *
- * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
- *//*
-
 @Configuration
+@ConditionalOnClass({ConsoleServlet.class, FF4jDispatcherServlet.class})
+@AutoConfigureAfter(FF4JConfiguration.class)
 public class FF4JWebConfiguration extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(FF4JWebConfiguration.class);
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ConsoleServlet ff4jConsoleServlet) {
+        return new ServletRegistrationBean(ff4jConsoleServlet, "/ff4j-console");
     }
 
     @Bean
-    public ServletRegistrationBean servletRegistrationBean(ConsoleServlet consoleServlet) {
-        return new ServletRegistrationBean(consoleServlet, "/ff4j-console/");
+    @ConditionalOnMissingBean
+    public ConsoleServlet getFF4jServlet(FF4j ff4j) {
+        ConsoleServlet ff4jConsoleServlet = new ConsoleServlet();
+        ff4jConsoleServlet.setFf4j(ff4j);
+        return ff4jConsoleServlet;
     }
 
     @Bean
-    public ConsoleServlet consoleServlet(FF4j ff4j, ServletConfig servletConfig) throws ServletException {
-        ConsoleServlet consoleServlet = new ConsoleServlet();
-        consoleServlet.setFf4j(ff4j);
-        consoleServlet.init(servletConfig);
-        return consoleServlet;
+    public ServletRegistrationBean ff4jDispatcherServletRegistrationBean(FF4jDispatcherServlet ff4jDispatcherServlet) {
+        return new ServletRegistrationBean(ff4jDispatcherServlet, "/ff4j-web-console/*");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FF4jDispatcherServlet getFF4jDispatcherServlet(FF4j ff4j) {
+        FF4jDispatcherServlet ff4jConsoleServlet = new FF4jDispatcherServlet();
+        ff4jConsoleServlet.setFf4j(ff4j);
+        return ff4jConsoleServlet;
     }
 }
-*/
