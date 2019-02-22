724710dc25699cdf58a9d10a0285841cd3cc174a
Paul <paul58914080@gmail.com>
2019-02-23 00:10:53 +0530

    updated the license header

--- ff4j-spring-boot-autoconfigure/src/main/kotlin/org/ff4j/spring/boot/autoconfigure/FF4JConfiguration.kt
+++ ff4j-spring-boot-autoconfigure/src/main/kotlin/org/ff4j/spring/boot/autoconfigure/FF4JConfiguration.kt
@@ -1,17 +1,22 @@
-/*
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * Copyright 2013-2016 the original author or authors.
+/*-
+ * #%L
+ * ff4j-spring-boot-autoconfigure
+ * %%
+ * Copyright (C) 2013 - 2019 FF4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
-
 package org.ff4j.spring.boot.autoconfigure
 
 import org.ff4j.FF4j
@@ -31,8 +36,7 @@ import org.springframework.context.annotation.Configuration
 @ComponentScan(value = ["org.ff4j.spring.boot.web.api", "org.ff4j.services", "org.ff4j.aop", "org.ff4j.spring"])
 class FF4JConfiguration {
 
-    val fF4j: FF4j
-        @Bean
-        @ConditionalOnMissingBean
-        get() = FF4j()
+    @Bean
+    @ConditionalOnMissingBean
+    fun getFF4J(): FF4j = FF4j()
 }
