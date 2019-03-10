b47d74433c0b63a647f9bb4e4868d249247fa1a0
paul58914080 <paul58914080@gmail.com>
2019-03-10 12:50:29 +0530

    feat(swagger): corrected example EnableFF4jSwagger to conditionally add swagger documentation

--- ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/config/SwaggerConfig.java
+++ ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/config/SwaggerConfig.java
@@ -1,21 +1,13 @@
 package org.ff4j.sample.config;
 
-import org.springframework.context.annotation.Bean;
+import org.ff4j.spring.boot.web.api.config.EnableFF4jSwagger;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
+@EnableFF4jSwagger
 public class SwaggerConfig {
-    @Bean
+    // if you wish to have your own configurations
+    /*@Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
@@ -36,5 +28,5 @@ public class SwaggerConfig {
                 "Apache 2.0",
                 "http://www.apache.org/licenses/LICENSE-2.0.html",
                 Collections.emptyList());
-    }
+    }*/
 }
