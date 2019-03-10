5f9c7153b59b76ccfe364d1c25c462eb51e1763d
paul58914080 <paul58914080@gmail.com>
2019-03-10 12:11:54 +0530

    feat(swagger): added github profile details for contact

--- ff4j-spring-boot-web-api/src/main/kotlin/org/ff4j/spring/boot/web/api/config/FF4jSwaggerConfig.kt
+++ ff4j-spring-boot-web-api/src/main/kotlin/org/ff4j/spring/boot/web/api/config/FF4jSwaggerConfig.kt
@@ -54,10 +54,10 @@ open class FF4jSwaggerConfig {
 
     private fun apiInfo(): ApiInfo {
         return ApiInfo("FF4J (ff4j.org) WebAPI",
-                "Administrate and operate all tasks on your features through this api.",
+                "Administer and operate all tasks on your features through this api.",
                 "1.8",
                 "Terms of service",
-                Contact("Paul WILLIAMS", "", "paul58914080@gmail.com"),
+                Contact("Paul WILLIAMS", "https://github.com/paul58914080", "paul58914080@gmail.com"),
                 "Apache 2.0",
                 "http://www.apache.org/licenses/LICENSE-2.0.html",
                 ArrayList())
