7444b9a3591ca3d3b0f7dc37581ee1994ef1dd40
paul58914080 <paul58914080@gmail.com>
2019-04-19 00:26:52 +0530

    refactor(web-api): fixed issue of unwanted global variable

--- ff4j-spring-boot-web-api/src/test/java/org/ff4j/spring/boot/web/api/config/SwaggerConfigTest.java
+++ ff4j-spring-boot-web-api/src/test/java/org/ff4j/spring/boot/web/api/config/SwaggerConfigTest.java
@@ -5,11 +5,10 @@ import org.junit.Test;
 import springfox.documentation.spring.web.plugins.Docket;
 
 public class SwaggerConfigTest {
-    private FF4jSwaggerConfig fixture;
 
     @Test
     public void testDocketCreation() {
-        fixture = new FF4jSwaggerConfig();
+        FF4jSwaggerConfig fixture = new FF4jSwaggerConfig();
         Docket docket = fixture.api();
         Assert.assertNotNull(docket);
     }
