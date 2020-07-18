2498d87fe43be5d68bbb37442e4c0bd64b42072c
Paul <paul58914080@gmail.com>
2020-07-18 13:15:55 +0530

    refactor(sample): removed old servlet console configuration

--- ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/config/FF4JWebConfiguration.java
+++ ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/config/FF4JWebConfiguration.java
@@ -2,7 +2,6 @@ package org.ff4j.sample.config;
 
 import org.ff4j.FF4j;
 import org.ff4j.web.FF4jDispatcherServlet;
-import org.ff4j.web.embedded.ConsoleServlet;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -10,24 +9,12 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ConditionalOnClass({ConsoleServlet.class, FF4jDispatcherServlet.class})
+@ConditionalOnClass({FF4jDispatcherServlet.class})
 @AutoConfigureAfter(FF4JConfiguration.class)
-public class FF4JWebConfiguration extends SpringBootServletInitializer {
-
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean(ConsoleServlet ff4jConsoleServlet) {
-        return new ServletRegistrationBean(ff4jConsoleServlet, "/ff4j-console");
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ConsoleServlet getFF4jServlet(FF4j ff4j) {
-        ConsoleServlet ff4jConsoleServlet = new ConsoleServlet();
-        ff4jConsoleServlet.setFf4j(ff4j);
-        return ff4jConsoleServlet;
-    }
+public class FF4JWebConfiguration extends SpringBootServletInitializer implements WebMvcConfigurer {
 
     @Bean
     public ServletRegistrationBean ff4jDispatcherServletRegistrationBean(FF4jDispatcherServlet ff4jDispatcherServlet) {
