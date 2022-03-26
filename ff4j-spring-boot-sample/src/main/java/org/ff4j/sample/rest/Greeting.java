9851a17b40914267443cecac8535ed3dfb642855
Paul Williams <paul58914080@gmail.com>
2022-03-26 13:28:19 +0530

    refactor: remove unwanted files

--- ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/rest/Greeting.java
+++ ff4j-spring-boot-sample/src/main/java/org/ff4j/sample/rest/Greeting.java
@@ -1,15 +1,24 @@
 package org.ff4j.sample.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Tag(name = "Greetings", description = "operations pertaining to greeting")
 public class Greeting {
 
   @GetMapping()
-  public String sayHello() {
-    return "Hello world";
+  @Operation(summary = "Greeting")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+  public String greeting() {
+    return "Hello World!";
   }
 }
