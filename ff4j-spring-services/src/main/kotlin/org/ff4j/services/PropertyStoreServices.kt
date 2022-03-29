e6b9a13f35e34db2ce674b07a2be0091ce6d92ee
Paul Williams <paul58914080@gmail.com>
2022-03-29 19:50:11 +0530

    docs: update license year to 2022

--- ff4j-spring-services/src/main/kotlin/org/ff4j/services/PropertyStoreServices.kt
+++ ff4j-spring-services/src/main/kotlin/org/ff4j/services/PropertyStoreServices.kt
@@ -2,7 +2,7 @@
  * #%L
  * ff4j-spring-services
  * %%
- * Copyright (C) 2013 - 2019 FF4J
+ * Copyright (C) 2013 - 2022 FF4J
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,30 +37,30 @@ import java.util.stream.Collectors
  */
 @Service
 class PropertyStoreServices(@Autowired val ff4j: FF4j) {
-    fun getPropertyStore(): PropertyStoreApiBean {
-        return PropertyStoreApiBean(ff4j.propertiesStore)
-    }
+  fun getPropertyStore(): PropertyStoreApiBean {
+    return PropertyStoreApiBean(ff4j.propertiesStore)
+  }
 
-    fun getAllProperties(): List<PropertyApiBean> {
-        val allProperties = ff4j.propertiesStore.readAllProperties()
-        return if (CollectionUtils.isEmpty(allProperties)) {
-            ArrayList(0)
-        } else {
-            allProperties.values.stream().map { PropertyApiBean(it) }.collect(Collectors.toList())
-        }
+  fun getAllProperties(): List<PropertyApiBean> {
+    val allProperties = ff4j.propertiesStore.readAllProperties()
+    return if (CollectionUtils.isEmpty(allProperties)) {
+      ArrayList(0)
+    } else {
+      allProperties.values.stream().map { PropertyApiBean(it) }.collect(Collectors.toList())
     }
+  }
 
-    fun deleteAllProperties() {
-        ff4j.propertiesStore.clear()
-    }
+  fun deleteAllProperties() {
+    ff4j.propertiesStore.clear()
+  }
 
-    fun getPropertiesFromCache(): CacheApiBean {
-        ff4j.cacheProxy ?: throw PropertyStoreNotCached()
-        return CacheApiBean(ff4j.propertiesStore)
-    }
+  fun getPropertiesFromCache(): CacheApiBean {
+    ff4j.cacheProxy ?: throw PropertyStoreNotCached()
+    return CacheApiBean(ff4j.propertiesStore)
+  }
 
-    fun clearCachedPropertyStore() {
-        val cacheProxy = ff4j.cacheProxy ?: throw PropertyStoreNotCached()
-        cacheProxy.cacheManager.clearProperties()
-    }
+  fun clearCachedPropertyStore() {
+    val cacheProxy = ff4j.cacheProxy ?: throw PropertyStoreNotCached()
+    cacheProxy.cacheManager.clearProperties()
+  }
 }
