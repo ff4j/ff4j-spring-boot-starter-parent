465b0ece3fee0a99c1ead7d192b7b86585e563ea
Paul Williams <paul58914080@gmail.com>
2023-05-28 09:47:21 +0200

    refactor(autoconfigure-webmvc): rename to FF4JWebMvcSecurityConfiguration

--- ff4j-spring-boot-autoconfigure-webmvc/src/main/kotlin/org/ff4j/spring/boot/autoconfigure/webmvc/FF4JWebMvcSecurityConfiguration.kt
+++ ff4j-spring-boot-autoconfigure-webmvc/src/main/kotlin/org/ff4j/spring/boot/autoconfigure/webmvc/FF4JWebMvcSecurityConfiguration.kt
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 @AutoConfiguration
 @AutoConfigureAfter(FF4JConfiguration::class)
 @ConditionalOnClass(FF4j::class)
-class FF4JWebMvcInitializationConfiguration(private val config: FF4JConfigurationProperties, val ff4j: FF4j) {
+class FF4JWebMvcSecurityConfiguration(private val config: FF4JConfigurationProperties, val ff4j: FF4j) {
 
   @PostConstruct
   fun init() {
